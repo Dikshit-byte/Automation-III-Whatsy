44ca7e7363b69aed5833b4abc96d095a91160682
Dikshit <singhdikshit69@gmail.com>
2023-01-28 19:58:39 +0530

    ditch gif project

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-// Supports ES6
-// import { create, Whatsapp } from 'venom-bot';
 const venom = require("venom-bot");
 
 venom
   .create({
-    session: "session-name", //name of session
-    multidevice: true, // for version not multidevice use false.(default: true)
+    session: "session-name",
+    multidevice: true,
   })
   .then((client) => start(client))
   .catch((erro) => {
@@ -18,43 +16,39 @@ function start(client) {
       client
         .sendText(message.from, "Welcome Venom 🕷")
         .then((result) => {
-          console.log("Result: ", result); //return object success
+          console.log("Result: ", result);
         })
         .catch((erro) => {
-          console.error("Error when sending: ", erro); //return object error
+          console.error("Error when sending: ", erro);
         });
     } else if (message.body === "A:" || message.body === "a:") {
       client
-        .sendVoice("918860439648@c.us", "./song.mp3")
+        .sendVoice(message.from, "./song.mp3")
         .then((result) => {
-          console.log("Result: ", result); //return object success
+          console.log("Result: ", result);
         })
         .catch((erro) => {
-          console.error("Error when sending: ", erro); //return object error
+          console.error("Error when sending: ", erro);
         });
     } else if (message.body === "I:" || message.body === "i:") {
       client
-        .sendImage("918860439648@c.us", "./diku.png", "diku", "My Photo")
+        .sendImage(message.from, "./diku.png", "diku", "My Photo")
         .then((result) => {
-          console.log("Result: ", result); //return object success
+          console.log("Result: ", result);
         })
         .catch((erro) => {
-          console.error("Error when sending: ", erro); //return object error
+          console.error("Error when sending: ", erro);
         });
     } else if (message.body === "F:" || message.body === "f:") {
       client
-        .sendFile(
-          "918860439648@c.us",
-          "./CV.pdf",
-          "CV",
-          "See my file in pdf"
-        )
+        .sendFile(message.from, "./CV.pdf", "CV", "See my file in pdf")
         .then((result) => {
-          console.log("Result: ", result); //return object success
+          console.log("Result: ", result);
         })
         .catch((erro) => {
-          console.error("Error when sending: ", erro); //return object error
+          console.error("Error when sending: ", erro);
         });
     }
   });
 }
+
