0a6645cafaa75199683f6da2bfefbae3156d8cf8
Dikshit <singhdikshit69@gmail.com>
2023-01-28 20:53:52 +0530

    add gpt-2.0

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
 const venom = require("venom-bot");
+const dotenv = require("dotenv");
+const {Configuration,OpenAIApi} = require("openai");
+
+dotenv.config();
+
+// OpenAI model Api
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+//& for normal searches or find the answer of any general query
+async function searchNotes(topic) {
+  const chatResponse = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: topic,
+    temperature: 0.3,
+    max_tokens: 1024,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.6,
+    stop: [" Human:", " AI:"],
+  });
+  return chatResponse.data.choices[0].text;
+}
+
+//^ For help in code
+// async function codex(text) {
+//   const code = await openai.createCompletion({
+//     model: "text-davinci-003",
+//     prompt: text,
+//     temperature: 0.1,
+//     max_tokens: 1024,
+//     top_p: 1.0,
+//     frequency_penalty: 0.0,
+//     presence_penalty: 0.2,
+//   });
+//   return code.data.choices[0].text;
+// }
+
 
 venom
   .create({
@@ -11,12 +51,21 @@ venom
   });
 
 function start(client) {
-  client.onMessage((message) => {
+  client.onMessage(async(message) => {
+    console.log(message.body);
+    const cutPiece = message.body.split(": ");
+    const tag = cutPiece[0]+": ";
+    const text = cutPiece[1];
     if (message.body === "Hi" && message.isGroupMsg === false) {
+      // await client.sendMessage(msg.from, "Hey there! I'm a bot and my boss will be available after 11 pm tonight. So keep ur messages to urself until 11😁");
+      ` 
+      1. Chat:  Chit chat with ai\n\n\t\tFor example:\nc: What's your name?\nC: what's your name?\n
+      2. Codex:  Get a solution of any programming question\n\n\t\tFor example:\nc: Write a code for linear search in c++\nC: Write a code for linear search in c++\n
+              `
       client
-        .sendText(message.from, "Welcome Venom 🕷")
+        .sendText(message.from, "Elliot")
         .then((result) => {
-          console.log("Result: ", result);
+          // console.log("Result: ", result);
         })
         .catch((erro) => {
           console.error("Error when sending: ", erro);
@@ -25,7 +74,7 @@ function start(client) {
       client
         .sendVoice(message.from, "./song.mp3")
         .then((result) => {
-          console.log("Result: ", result);
+          // console.log("Result: ", result);
         })
         .catch((erro) => {
           console.error("Error when sending: ", erro);
@@ -34,7 +83,7 @@ function start(client) {
       client
         .sendImage(message.from, "./diku.png", "diku", "My Photo")
         .then((result) => {
-          console.log("Result: ", result);
+          // console.log("Result: ", result);
         })
         .catch((erro) => {
           console.error("Error when sending: ", erro);
@@ -43,7 +92,17 @@ function start(client) {
       client
         .sendFile(message.from, "./CV.pdf", "CV", "See my file in pdf")
         .then((result) => {
-          console.log("Result: ", result);
+          // console.log("Result: ", result);
+        })
+        .catch((erro) => {
+          console.error("Error when sending: ", erro);
+        });
+    } else if(tag === "Q: " || tag === "q: "){
+      let result = await searchNotes(text);
+      client
+        .sendText(message.from, result)
+        .then((result) => {
+          // console.log("Result: ", result);
         })
         .catch((erro) => {
           console.error("Error when sending: ", erro);
