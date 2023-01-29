7de6f773da2c534f91c9db1e3c45bbd431203dda
Dikshit <singhdikshit69@gmail.com>
2023-01-30 00:10:18 +0530

    signin off

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const venom = require("venom-bot");
 const dotenv = require("dotenv");
-const {Configuration,OpenAIApi} = require("openai");
-const wget = require("node-wget");
+const { Configuration, OpenAIApi } = require("openai");
+// const wget = require("node-wget");
 // const PythonShell = require('python-shell').PythonShell;
-const exec = require('child_process').exec;
-const download = require('image-downloader');
+// const exec = require("child_process").exec;
+const download = require("image-downloader");
 
 dotenv.config();
 
 var currentPath = process.cwd();
 // OpenAI model Api
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -20,7 +20,7 @@ async function searchNotes(topic) {
   const chatResponse = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: topic,
-    temperature: 0.5,
+    temperature: 0.3,
     max_tokens: 1024,
     top_p: 1.0,
     frequency_penalty: 0.0,
@@ -30,30 +30,15 @@ async function searchNotes(topic) {
   return chatResponse.data.choices[0].text;
 }
 
-function dlImg(url, filepath) {
-  let imgName;
-  options = {
-    url: url,
-    dest: filepath+"\\img",         // will be saved to /path/to/dest/photo
-    extractFilename: true,
-  };
-  download.image(options)
-  .then(({ filename }) => {
-    imgName = filename
-  }).catch((err) => console.error(err));
-  return imgName;
-}
-
-async function searchImage(topic){
+async function searchImage(topic) {
   const imgResponse = await openai.createImage({
     prompt: topic,
     n: 1,
     size: "1024x1024",
   });
   image_url = imgResponse.data.data[0].url;
-  let result = dlImg(image_url,currentPath);
-  console.log(result);
-  // return result;
+  console.log(image_url);
+  return image_url;
 }
 
 
@@ -71,7 +56,8 @@ async function searchImage(topic){
 //   return code.data.choices[0].text;
 // }
 
-venom.create({
+venom
+  .create({
     session: "session-name",
     multidevice: true,
   })
@@ -81,59 +67,69 @@ venom.create({
   });
 
 function start(client) {
-  client.onMessage(async(message) => {
+  client.onMessage(async (message) => {
     console.log(message.body);
     const cutPiece = message.body.split(": ");
-    const tag = cutPiece[0]+": ";
+    const tag = cutPiece[0] + ": ";
     const text = cutPiece[1];
     if (message.body === "Hi" && message.isGroupMsg === false) {
       // await client.sendMessage(msg.from, "Hey there! I'm a bot and my boss will be available after 11 pm tonight. So keep ur messages to urself until 11😁");
       ` 
       1. Chat:  Chit chat with ai\n\n\t\tFor example:\nc: What's your name?\nC: what's your name?\n
       2. Codex:  Get a solution of any programming question\n\n\t\tFor example:\nc: Write a code for linear search in c++\nC: Write a code for linear search in c++\n
-      `
+      `;
       client
-        .sendText(message.from, "Elliot")
+        .sendText(message.from, "Hey bruh!!")
         .then((result) => {
           // console.log("Result: ", result);
         })
         .catch((erro) => {
           console.error("Error when sending: ", erro);
         });
-    } 
-    
-    switch(tag){
+    }
 
+    switch (tag) {
       //& for audio files
-      case "A: "||"a: ":
+      case "A: ":
+        case "a: ":
         client
-        .sendVoice(message.from, "./song.mp3")
-        .then((result) => {
-          // console.log("Result: ", result);
-        })
-        .catch((erro) => {
-          console.error("Error when sending: ", erro);
-        });
+          .sendVoice(message.from, "./song.mp3")
+          .then((result) => {
+            // console.log("Result: ", result);
+          })
+          .catch((erro) => {
+            console.error("Error when sending: ", erro);
+          });
         break;
-      
 
       // ! for images
-      case "I: "|| "i: ":
+      case "I: ":
+        case "i: ":
         let img = await searchImage(text);
-        client
-        // .sendImage (message.from, img, text)
-        .sendText(message.from, "Image")
-        .then((result) => {
-          // console.log("Result: ", result);
-        })
-        .catch((erro) => {
-          console.error("Error when sending: ", erro);
-        });
-
+        async function dlImg(url, filepath) {
+          options = {
+            url: url,
+            dest: filepath + "\\img",
+            extractFilename: true,
+          };
+          await download
+            .image(options)
+            .then(({ filename }) => {
+              console.log(filename);
+              client
+                .sendImage(message.from, filename, text)
+                .catch((erro) => {
+                  console.error("Error when sending: ", erro);
+                });
+            })
+            .catch((err) => console.error(err));
+        }
+        dlImg(img,currentPath);
         break;
 
       //* for documents
-      case "F: "||"f: ":
+      case "F: ":
+        case "f: ":
         client
           .sendFile(message.from, "./CV.pdf", "CV", "See my file in pdf")
           .then((result) => {
@@ -144,10 +140,9 @@ function start(client) {
           });
         break;
 
-
-      
       // ~ for GPT-2.0
-      case "Q: "||"q: ":
+      case "Q: ":
+        case "q: ":
         let result = await searchNotes(text);
         client
           .sendText(message.from, result)
@@ -157,8 +152,7 @@ function start(client) {
           .catch((erro) => {
             console.error("Error when sending: ", erro);
           });
-          break;
+        break;
     }
   });
 }
-
